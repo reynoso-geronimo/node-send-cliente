7489548cc63fe27bbb8880092c817a6ebd87a6ce
reynoso-geronimo <86173823+reynoso-geronimo@users.noreply.github.com>
2021-09-22 12:53:08 -0300

    preparando Dropzone

---
+++ components/Dropzone.js
@@ -0,0 +1,39 @@
+import React, { useState, useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
+import clienteAxios from "../config/axios";
+
+const Dropzone = () => {
+  //extraer contenido de dropzone
+  const onDrop=(acceptedFiles)=>{
+      console.log(acceptedFiles)
+  } 
+
+  const { getRootProps, getInputProps, isDragActive,acceptedFiles } = useDropzone({onDrop});
+
+  return (
+    <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0 flex flex-col items-center justify-center border-dasehd border-gray-400 border-2 bg-gray-100 px-4">
+      <div {...getRootProps({ className: "dropzone w-full py-32" })}>
+        <input className="h-100" {...getInputProps()} />
+
+        {isDragActive ? (
+          <p className='text-2xl text-center text-gray-600'>Suelta el Archivo</p>
+        ) : (
+          <div className="text-center">
+            <p className="text-2xl text-center text-gray-600">
+              Seleccion un archivo y arrastralo aqui
+            </p>
+            <button
+              className="bg-blue-700 w-full py-3 rounded-lg text-white my-10 hover:bg-blue-800"
+              type="button"
+            >
+              Selecciona archivos para subir
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dropzone;
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,41 @@
-import React,{useContext, useEffect} from 'react';
-import Layout from '../components/Layout';
-import authContext from '../context/auth/authContext';
-
-
+import React, { useContext, useEffect } from "react";
+import Layout from "../components/Layout";
+import authContext from "../context/auth/authContext";
+import Link from "next/link";
+import Dropzone from "../components/Dropzone";
 
 const Index = () => {
+  const AuthContext = useContext(authContext);
+  const { usuarioAutenticado } = AuthContext;
 
-  const AuthContext = useContext(authContext)
-  const {usuarioAutenticado}= AuthContext
- 
   useEffect(() => {
-    usuarioAutenticado()
-  }, [])
+    usuarioAutenticado();
+  }, []);
 
-  return ( <Layout>
-    <h1>Desde el Index</h1>
-  </Layout> );
-}
- 
+  return (
+    <Layout>
+      <div className="md:w-4/5 xl:w-3/5 mx-auto mb-32">
+      <div className="lg:flex md:shadow-lg p-5 bg-white rounded-lg py-10">
+          <Dropzone/>
+          <div className="md:flex-1 mb-3 mx-2 mt-16 lg:mt-0">
+            <h2 className="text-4xl font-sans font-bold text-gray-800 my-4">
+              Compartir archivos de forma sencilla y privada
+            </h2>
+            <p className="text-lg leading-loose">
+              <span className="text-red-500 font-bold">ReactNodeSend</span> te
+              permite compartir archivos con cifrado de extremo a extremo y un
+              archivo que es eliminado después de ser descargado. Así que puedes
+              mantener lo que compartes en privado y asegurarte de que tus cosas
+              no permanezcan en línea para siempre.
+            </p>
+            <Link href='/crearcuenta'>
+              <a className='text-red-500 font-bold text-lg hover:text-red-700'>Crea una cuenta para mayores beneficios</a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
 export default Index;
